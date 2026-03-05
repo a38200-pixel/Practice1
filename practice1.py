82e48190b70406d72b7fb0a42efcd1a36478d570
이동현 <a38200@gmail.com>
2026-03-05 19:21:56 +0900

    3일차 수업 끝

--- practice1.py
+++ practice1.py
@@ -40,3 +40,42 @@ def multi(num):
 a = int(input("20:"))
 res = multi(a)
 print(res)
+
+#2부터 시작하는 구구단
+A = int(input("2-9숫자" ))
+if 1< A <10:
+    B = 1
+    while True:
+        if B<10:
+            C=A*B
+            print(A,"*",B,"=",C)
+            B +=1
+        else:
+            break
+print("끝")
+
+#위에 코드 단순화
+A = int(input("2-9숫자: "))
+
+if 1 < A < 10:
+    for B in range(1, 10):  # B를 1부터 9까지 자동으로 반복!
+        print(A, "*", B, "=", A * B)
+
+print("끝")
+
+A = int(input("2-9숫자: "))
+
+#더하기로 구구단 만들기
+if 1 < A < 10:
+    for B in range(1, 10):
+        # 곱셈 결과(C)를 0으로 초기화
+        C = 0
+        
+        # A를 B번 더하는 과정
+        for _ in range(B):
+            C += A
+            
+        print(A, "*", B, "=", C)
+
+print("끝")
+#3일차 수업 끝
